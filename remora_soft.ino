26c0dd25f20a109cfd39ad79b42defe6691395f7
Charles <hallard04@free.fr>
2015-12-14 11:08:00 +0100

    Changement casse #include WifiUdp.h

--- remora_soft.ino
+++ remora_soft.ino
@@ -44,7 +44,7 @@
   #include <ESP8266WiFi.h>
   #include <ESP8266WebServer.h>
   #include <ESP8266mDNS.h>
-  #include <WiFiUDP.h>
+  #include <WiFiUdp.h>
   #include <ArduinoOTA.h>
   #include <Wire.h>
   #include <SPI.h>
